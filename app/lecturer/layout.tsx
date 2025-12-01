9afafb90b362bd1a080ca8dc3cbded1c3b3d9087
filbertttt <filbertfilbert10@gmail.com>
2025-12-01 21:46:35 +0700

    Add Due Date when lecturer create assignment

--- app/lecturer/layout.tsx
+++ app/lecturer/layout.tsx
@@ -18,14 +18,14 @@ export default function LecturerLayout({ children }: { children: ReactNode }) {
               <div className="flex flex-col">
                 <Panel>
                   <div className="text-gray-900">
-                    <h2 className="text-2xl font-bold mb-4">Calendar</h2>
+                    <h2 className="text-2xl font-bold mb-4">Courses</h2>
                     <div className="bg-white p-3 rounded-lg shadow border-l-4 border-blue-500 mb-3">
-                      <p className="font-semibold">Today's Focus:</p>
-                      <p className="text-sm text-gray-600">Complete Module 3 Quiz</p>
+                      <p className="font-semibold">Dummy Courses</p>
+                      <p className="text-sm text-gray-600">Desc...</p>
                     </div>
-                    <div className="bg-white p-3 rounded-lg shadow border-l-4 border-yellow-500 mb-3">
-                      <p className="font-semibold">Upcoming Due Date:</p>
-                      <p className="text-sm text-gray-600">Project Proposal (Friday)</p>
+                    <div className="bg-white p-3 rounded-lg shadow border-l-4 border-blue-500 mb-3">
+                      <p className="font-semibold">Dummy Courses</p>
+                      <p className="text-sm text-gray-600">Desc...</p>
                     </div>
                     <div className="flex justify-center mt-auto p-4">
                       <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-150">
